678c29595a20fe1e73ac8a603d8c5def24b1155e
caitbrock <cait2brock@gmail.com>
2022-08-18 14:06:50 -0700

    Heroku testing

--- controllers/images.js
+++ controllers/images.js
@@ -1,21 +1,2 @@
 const aws = require("aws-sdk");
 const config = require("./config.json");
-// async function () {
-//     try{
-//         aws.config.setPromisesDependency();
-//         aws.config.update({
-//             accessKeyId: config.accessKey,
-//             secretAccessKey: config.aws.secretKey,
-//             region: 'us-east-1'
-//         })
-
-//         const s3 = new aws.S3();
-//         const response = await s3.listObjectsV2({
-//             Bucket: 'sqb-sample-2018',
-//             Prefix: 'folder1'
-//         }).promise();
-//         console.log(response)
-
-// }catch (e){
-//     console.log('our error', e)
-// }}
