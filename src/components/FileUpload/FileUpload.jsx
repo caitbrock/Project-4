40c7a2a561c22a287381684260babba1518deedb
Joon Suh (Jason) Sohn <sjs2240@Joons-MBP.hitronhub.home>
2022-08-18 16:27:13 -0400

    backend aws s3

--- src/components/FileUpload/FileUpload.jsx
+++ src/components/FileUpload/FileUpload.jsx
@@ -1,68 +1,16 @@
-import React, { useState } from "react";
-import AWS from "aws-sdk";
-import "./FileUpload.css";
-import { render } from "@testing-library/react";
-const S3_BUCKET = process.env.REACT_APP_BUCKET_NAME;
-const REGION = process.env.REACT_APP_REGION;
+import React, { Component } from "react";
 
-AWS.config.update({
-  accessKeyId: process.env.REACT_APP_ACCESS,
-  secretAccessKey: process.env.REACT_APP_SECRET,
-});
-
-const myBucket = new AWS.S3({
-  params: { Bucket: S3_BUCKET },
-  region: REGION,
-});
-
-const UploadImageToS3WithNativeSdk = () => {
-  const [progress, setProgress] = useState(0);
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const handleFileInput = (e) => {
-    setSelectedFile(e.target.files[0]);
-  };
-
-  const handleUploadFile = (file) => {
-    const params = {
-      ACL: "public-read",
-      Body: file,
-      Bucket: S3_BUCKET,
-      Key: file.name,
-    };
-
-    myBucket
-      .putObject(params)
-      .on("httpUploadProgress", (evt) => {
-        setProgress(Math.round((evt.loaded / evt.total) * 100));
-      })
-      .send((err) => {
-        if (err) console.log(err);
-      });
-  };
-
-  return (
-    <div className="upload-container">
-      <div className="create-post-header">Create Post</div>
-      <br></br>
-      <input
-        className="choose-file"
-        type="file"
-        multiple="true"
-        onChange={handleFileInput}
-        accept=".png,.jpg,.jpeg,.wmv,.avi"
-      />
-      <br></br>
-      <div>Uploading... {progress}%</div>
-      <br></br>
-      <button
-        className="post-btn"
-        onClick={() => handleUploadFile(selectedFile)}
-      >
-        Upload
-      </button>
-    </div>
-  );
-};
-
-export default UploadImageToS3WithNativeSdk;
+export default class FileUpload extends Component {
+  render() {
+    return (
+      <div className="container">
+        <form action="/" enctype="multipart/form-data" method="post">
+          <div class="form-group">
+            <input type="file" class="form-control-file" name="file" />
+            <input type="submit" value="Upload!" class="upload-botton" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
