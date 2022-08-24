235c6133419197f88c4b3bffb9073683def2b012
Joon Suh (Jason) Sohn <sohn2240@gmail.com>
2022-08-24 03:50:27 -0400

    merge

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -9,31 +9,37 @@ const ListItem = styled("li")(({ theme }) => ({
 
 function Images(props) {
   return (
-  <>
-    {props.posts.map((c) => (
-    <div className="imagecard" style={{ margin: "20px", maxWidth: 350, minWidth: 350}}>
-      <div className="imagecontent">
-        <div className="upload-image" component="img"></div>
-        <div className="image-title"> <h5>{c.title}</h5></div>
-        <div className="image-desription"> <h6>{c.destination}</h6></div>
-        <div className="image-desription"> <h6>{c.description}</h6></div>
-      </div>
-    
-    {c.tags.map((m) => (
-      <div className="imagetags">{m.tags}</div>
-    ))}
-    
-    </div>
-      )
-      )
-      }
-  </>
-  )}
+    <>
+      {props.posts.map((c) => (
+        <div
+          className="imagecard"
+          style={{ margin: "20px", maxWidth: 350, minWidth: 350 }}
+        >
+          <div className="imagecontent">
+            <div className="upload-image" component="img">
+              <img src={url} />
+            </div>
+            <div className="image-title">
+              {" "}
+              <h5>{c.title}</h5>
+            </div>
+            <div className="image-desription">
+              {" "}
+              <h6>{c.destination}</h6>
+            </div>
+            <div className="image-desription">
+              {" "}
+              <h6>{c.description}</h6>
+            </div>
+          </div>
 
-
-  
+          {c.tags.map((m) => (
+            <div className="imagetags">{m.tags}</div>
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
 
 export default Images;
-
-
-
