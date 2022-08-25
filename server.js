ffbf8f98e5a7036594f7aef3af7dcb1b26a4d348
RamSritharan <ramkishan1795@gmail.com>
2022-08-25 15:26:32 -0400

    Moving aws around

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 
-const { S3Client } = require("@aws-sdk/client-s3");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const AWS = require("aws-sdk");
-
 require("dotenv").config();
 require("./config/database.js");
 
@@ -43,27 +38,5 @@ io.on("connection", (socket) => {
   });
 });
 
-//AWS Upload//
-const s3 = new S3Client({
-  accessKeyId: process.env.REACT_APP_ACCESS,
-  secretAccessKey: process.env.REACT_APP_SECRET,
-  region: "ca-central-1",
-});
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.REACT_APP_BUCKET_NAME,
-    acl: "public-read",
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString());
-    },
-  }),
-});
-
-app.post("/", upload.single("file"), function (req, res, next) {
-  console.log("Uploaded!!!");
-});
+const imageUpload = require("./routes/api/aws");
+app.use("/", imageUpload);
